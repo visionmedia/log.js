406fea4f4180775c0bd915920e4f99163eb58b6f
Couto <lcouto87@gmail.com>
2012-02-12 01:19:40 +0000

    Colorful stdout, with ability to turn colors off/on

--- examples/file.js
+++ examples/file.js
@@ -6,12 +6,18 @@
 var Log = require('../lib/log')
   , fs = require('fs')
   // , stream = fs.createWriteStream(__dirname + '/file.log', { flags: 'a' })
-  , log = new Log('debug');
+  , log = new Log('debug').colorful({
+    DEBUG : '\033[1;30m',
+    NOTICE: '\033[0;32m'
+  });
 
 log.debug('a debug message');
 log.info('a info message');
 log.notice('a notice message');
 log.warning('a warning message');
+
+log.colorful(false);
+
 log.error('a error message');
 log.critical('a critical message');
 log.alert('a alert message');
