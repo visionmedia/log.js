2c8b2c9ab61392b3d506645033c07ba93909a597
neekey <ni184775761@gmail.com>
2012-08-03 21:15:48 +0800

    add method `customLevels` for customizing levels and `setLevel` to set current level

--- examples/stdout-color.js
+++ examples/stdout-color.js
@@ -4,12 +4,20 @@
  */
 
 var Log = require('../lib/log')
-  , log = new Log('notice').colorful();
+  , log = new Log('trace').colorful();
 
+/**
+ * Default log.
+ */
+log.trace( 'Below is default logs.');
 log.debug('a debug message');
 log.info('a info message');
 log.notice('a notice message');
 log.warning('a warning message');
+log.error('a error message');
+log.critical('a critical message');
+log.alert('a alert message');
+log.emergency('a emergency %s', 'message');
 
 /**
  * Setup our own colors
@@ -19,13 +27,40 @@ log.colorful({
     CRITICAL : "\033[1;36m"
 });
 
+log.trace( 'Below is color-customed logs.');
+log.debug('a debug message');
+log.info('a info message');
+log.notice('a notice message');
+log.warning('a warning message');
 log.error('a error message');
 log.critical('a critical message');
 log.alert('a alert message');
+log.emergency('a emergency %s', 'message');
 
 /**
- * Turn colors off
+ * Turn off color.
  */
-log.colorful(false);
+log.colorful( false );
+log.trace( 'Below is color-offed logs.');
+log.debug('a debug message');
+log.info('a info message');
+log.notice('a notice message');
+log.warning('a warning message');
+log.error('a error message');
+log.critical('a critical message');
+log.alert('a alert message');
 log.emergency('a emergency %s', 'message');
-log.emergency('a really %s emergency %s', 'bad', 'message');
+
+/**
+ * Customize levels and colors
+ */
+log.customLevels({
+    COMMAND: "\x1b[0;35m",
+    DATA: "\x1b[0;33m",
+    RESULT: "\x1b[0;36m"
+}).colorful( true).setLevel( 'result' );
+
+log.command( 'a command message' );
+log.data( 'a data message' );
+log.result( 'a result message' );
+
